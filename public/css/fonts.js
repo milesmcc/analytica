9ec50612bea61f1c2a635e7b9547e107dbc97e7e
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2017-08-10 16:53:08 -0400

    Changed color scheme and fonts

---
+++ public/css/fonts.js
@@ -0,0 +1,8 @@
+var MTIProjectId='04320a65-09fb-4a5d-adb9-ed8bd162c8d6';
+ (function() {
+        var mtiTracking = document.createElement('script');
+        mtiTracking.type='text/javascript';
+        mtiTracking.async='true';
+         mtiTracking.src='mtiFontTrackingCode.js';
+        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
+   })();
